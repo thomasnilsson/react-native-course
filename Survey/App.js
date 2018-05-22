d977ee538a39836a4378fbf8fc19643bfdea19b5
thomasnilsson <s144470@student.dtu.dk>
2018-05-22 15:58:01 +0200

    Callback works

--- Survey/App.js
+++ Survey/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Question from "./components/Question1.js";
 
 export default class App extends React.Component {
+  answerHandler = () => {
+    alert("answerhandler called");
+  };
+  
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    let q = {
+      text: "This is a question text",
+      answers: ["Answer One", "Answer Two", "Answer Three"]
+    };
+
+    return <Question question={q} onAnswerGiven={this.answerHandler} />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Survey/components/Question1.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+class Question extends React.Component {
+  state = {
+    answer: ""
+  };
+
+  render() {
+    let answers = this.props.question.answers;
+    let text = this.props.question.text;
+
+    answerGiven = answer => {
+      this.props.onAnswerGiven();
+      // alert("test")
+      console.log(this.props)
+      this.setState({
+        state: answer
+      });
+    };
+
+    let buttons = answers.map((a, i) => (
+      <Button key={i} title={a} onPress={() => answerGiven(a)} />
+    ));
+    return (
+      <View style={styles.parentContainer}>
+        <View style={styles.questionContainer}>
+          <Text style={{ fontSize: 28 }}>{text}</Text>
+        </View>
+        <View style={styles.buttonContainer}>{buttons}</View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  parentContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    margin: 20
+  },
+  questionContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 60
+  },
+  buttonContainer: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
+
+// export default question;
+export default Question;
